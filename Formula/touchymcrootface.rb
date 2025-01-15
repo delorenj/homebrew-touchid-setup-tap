45d6af35cef850eb3e1915746cda47cdc4e38509
Jarad DeLorenzo <jaradd@gmail.com>
2025-01-14 23:33:52 -0500

    Update SHA256 hash for ARM64 binary

--- Formula/touchymcrootface.rb
+++ Formula/touchymcrootface.rb
@@ -7,7 +7,7 @@ class Touchymcrootface < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/delorenj/touchymcrootface/releases/download/v1.0.0/touchymcrootface-darwin-arm64"
-      sha256 "81da448f71922591e477cd6a011a18223f00269385a9c7e1056fd8ec1b91f9b9"
+      sha256 "e713aafaa005a39340441f95fa99561cfb405478be7870e0bb9cfb24f3bc1d58"
     else
       url "https://github.com/delorenj/touchymcrootface/releases/download/v1.0.0/touchymcrootface-darwin-amd64"
       sha256 "d78d583ef3e3b44243f1c70555e73658224799e17a97f101b95cc1fedbac4141"
